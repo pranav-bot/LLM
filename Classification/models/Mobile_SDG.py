b0b678b67392ee8ff9b580edcdaad7a383c868e6
Sana-0511 <svnkanbargi@gmail.com>
2024-05-18 18:09:09 +0530

    SVM,SDG added

--- Classification/models/Mobile_SDG.py
+++ Classification/models/Mobile_SDG.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
@@ -9,14 +9,15 @@ df = pd.read_csv('Classification/preprocessed_data/MobilePrice.csv')
 # Define target variable
 target_variable = "price_range"
 
-# Function to train logistic regression model
-def LogisticRegressionModel(df, target_variable, fit_intercept=True, solver='liblinear'):
+# Function to train SGD model
+def SGDClassifierModel(df, target_variable, loss='log_loss', max_iter=1000, tol=1e-3):
     X = df.drop(columns=[target_variable])
     y = df[target_variable]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = LogisticRegression(fit_intercept=fit_intercept, solver=solver)
+    model = SGDClassifier(loss=loss, max_iter=max_iter, tol=tol, random_state=42)
     model.fit(X_train, y_train)
     return model
+
 
 # Function to evaluate classification
 def evaluate_classification(model, df):
@@ -30,14 +31,14 @@ def evaluate_classification(model, df):
     f1 = f1_score(y_test, y_pred, average='weighted')
     return accuracy, precision, recall, f1
 
-# Train logistic regression model
-model = LogisticRegressionModel(df, target_variable=target_variable)
+# Train SGD model
+model = SGDClassifierModel(df, target_variable=target_variable)
 
 # Evaluate the model
 accuracy, precision, recall, f1 = evaluate_classification(model, df)
 
 # Print evaluation metrics
-print("Accuracy for the Logistic Regression model: {:.2f}".format(accuracy))
-print("Precision for the Logistic Regression model: {:.2f}".format(precision))
-print("Recall for the Logistic Regression model: {:.2f}".format(recall))
-print("F1 Score for the Logistic Regression model: {:.2f}".format(f1))
+print("Accuracy for the SGD Classifier model: {:.2f}".format(accuracy))
+print("Precision for the SGD Classifier model: {:.2f}".format(precision))
+print("Recall for the SGD Classifier model: {:.2f}".format(recall))
+print("F1 Score for the SGD Classifier model: {:.2f}".format(f1))
